feac9c21cf5354d20a8eb9473401fa4faedc67ab
Tony Seebregts <tony.seebregts@gmail.com>
2019-07-29 21:22:40 -0700

    Skeleton simulator REST swipe implementation

--- src/uhppote-simulator/rest/rest.go
+++ src/uhppote-simulator/rest/rest.go
@@ -1,33 +1,52 @@
 package rest
 
 import (
+	"encoding/json"
 	"fmt"
 	"html"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
+	"strconv"
 	"uhppote-simulator/simulator"
+	"uhppote/types"
 )
+
+type AccessCard struct {
+	CardNumber uint32 `json:"card-number"`
+}
+
+type Swipe struct {
+	Card AccessCard `json:"card"`
+}
+
+type context struct {
+	simulators []*simulator.Simulator
+}
 
 type handler struct {
 	re *regexp.Regexp
-	fn http.HandlerFunc
+	fn func(*context, http.ResponseWriter, *http.Request)
 }
 
 type dispatcher struct {
+	ctx      *context
 	handlers []handler
 }
 
 func Run(simulators []*simulator.Simulator) {
 	d := new(dispatcher)
 
+	d.ctx = new(context)
+	d.ctx.simulators = simulators
+
 	d.Add("^/uhppote/simulator/[0-9]+/door/[1-4]/swipe$", swipe)
 
 	log.Fatal(http.ListenAndServe(":8080", d))
 }
 
-func (d *dispatcher) Add(path string, h http.HandlerFunc) {
+func (d *dispatcher) Add(path string, h func(*context, http.ResponseWriter, *http.Request)) {
 	re := regexp.MustCompile(path)
 	d.handlers = append(d.handlers, handler{re, h})
 }
@@ -46,7 +65,7 @@ func (d *dispatcher) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	url := r.URL.Path
 	for _, h := range d.handlers {
 		if h.re.MatchString(url) {
-			h.fn(w, r)
+			h.fn(d.ctx, w, r)
 			return
 		}
 	}
@@ -55,7 +74,7 @@ func (d *dispatcher) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Unsupported API", http.StatusBadRequest)
 }
 
-func swipe(w http.ResponseWriter, r *http.Request) {
+func swipe(ctx *context, w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, fmt.Sprintf("Invalid method:%s - expected POST", r.Method), http.StatusBadRequest)
 		return
@@ -63,16 +82,56 @@ func swipe(w http.ResponseWriter, r *http.Request) {
 
 	url := r.URL.Path
 	matches := regexp.MustCompile("^/uhppote/simulator/([0-9]+)/door/([1-4])/swipe$").FindStringSubmatch(url)
-	deviceId := matches[1]
-	door := matches[2]
+	deviceId, _ := strconv.ParseUint(matches[1], 10, 32)
+	door, _ := strconv.ParseUint(matches[2], 10, 8)
 
-	json, err := ioutil.ReadAll(r.Body)
+	blob, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(deviceId)
-	fmt.Println(door)
-	fmt.Println(json)
-	fmt.Fprintf(w, "Greeting earthling, %q", html.EscapeString(r.URL.Path))
+	var swipe Swipe
+	err = json.Unmarshal(blob, &swipe)
+	if err != nil {
+		http.Error(w, "Invalid request format", http.StatusBadRequest)
+	} else {
+		fmt.Printf("SWIPE: %v\n", swipe)
+		fmt.Println(deviceId)
+		fmt.Println(door)
+		fmt.Println(string(blob))
+
+		for _, s := range ctx.simulators {
+			if s.SerialNumber == types.SerialNumber(deviceId) {
+				for _, c := range s.Cards {
+					if c.CardNumber == swipe.Card.CardNumber {
+						switch door {
+						case 1:
+							if c.Door1 {
+								fmt.Fprintf(w, "Greeting earthling, %q", html.EscapeString(r.URL.Path))
+								return
+							}
+						case 2:
+							if c.Door2 {
+								fmt.Fprintf(w, "Greeting earthling, %q", html.EscapeString(r.URL.Path))
+								return
+							}
+						case 3:
+							if c.Door3 {
+								fmt.Fprintf(w, "Greeting earthling, %q", html.EscapeString(r.URL.Path))
+								return
+							}
+						case 4:
+							if c.Door4 {
+								fmt.Fprintf(w, "Greeting earthling, %q", html.EscapeString(r.URL.Path))
+								return
+							}
+
+						}
+					}
+				}
+			}
+		}
+
+		fmt.Fprintf(w, "Access denied")
+	}
 }
