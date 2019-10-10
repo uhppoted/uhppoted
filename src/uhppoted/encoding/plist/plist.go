2fa7dcee94a1cec5d7c95e0dcfef3ba59208ce11
Tony Seebregts <tony.seebregts@gmail.com>
2019-10-10 13:11:04 -0700

    uhppoted: Basic launchd plist decoder implementation

--- src/uhppoted/encoding/plist/plist.go
+++ src/uhppoted/encoding/plist/plist.go
@@ -29,13 +29,21 @@ var body = xml.StartElement{
 		xml.Attr{xml.Name{"", "version"}, "1.0"}},
 }
 
-type encoding func(*xml.Encoder, reflect.Value) error
+type encoder func(*xml.Encoder, reflect.Value) error
+type decoder func(*xml.Decoder, string, reflect.Value) error
 
-var dispatch = map[reflect.Type]encoding{
+var encoders = map[reflect.Type]encoder{
 	reflect.TypeOf(string("")):  encodeString,
 	reflect.TypeOf(bool(false)): encodeBool,
 	reflect.TypeOf(int(0)):      encodeInt,
 	reflect.TypeOf([]string{}):  encodeStringArray,
+}
+
+var decoders = map[reflect.Type]decoder{
+	reflect.TypeOf(string("")):  decodeString,
+	reflect.TypeOf(bool(false)): decodeBool,
+	reflect.TypeOf(int(0)):      decodeInt,
+	reflect.TypeOf([]string{}):  decodeStringArray,
 }
 
 func Encode(p interface{}) ([]byte, error) {
@@ -79,7 +87,7 @@ func encode(s reflect.Value) ([]byte, error) {
 				return buffer.Bytes(), err
 			}
 
-			if g, ok := dispatch[t.Type]; ok {
+			if g, ok := encoders[t.Type]; ok {
 				if err := g(encoder, f); err != nil {
 					return buffer.Bytes(), err
 				}
@@ -147,6 +155,295 @@ func encodeStringArray(e *xml.Encoder, f reflect.Value) error {
 	return nil
 }
 
-func Decode(bytes []byte) (interface{}, error) {
-	return nil, nil
+func Decode(b []byte, p interface{}) error {
+	decoder := xml.NewDecoder(bytes.NewReader(b))
+
+	for {
+		token, err := decoder.Token()
+		if err != nil {
+			return err
+		}
+
+		element, ok := token.(xml.StartElement)
+		if ok {
+			if element.Name.Local == "plist" {
+				break
+			}
+		}
+	}
+
+	for {
+		token, err := decoder.Token()
+		if err != nil {
+			return err
+		}
+
+		if element, ok := token.(xml.StartElement); ok {
+			if element.Name.Local == "dict" {
+				break
+			}
+		}
+	}
+
+	for {
+		token, err := decoder.Token()
+		if err != nil {
+			return err
+		}
+
+		if element, ok := token.(xml.EndElement); ok {
+			if element.Name.Local == "dict" {
+				break
+			}
+		}
+
+		if element, ok := token.(xml.StartElement); ok {
+			if element.Name.Local == "key" {
+				key, err := decodeKey(decoder)
+				if err != nil {
+					return err
+				}
+
+				err = decodeValue(decoder, key, p)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	for {
+		token, err := decoder.Token()
+		if err != nil {
+			return err
+		}
+
+		if element, ok := token.(xml.EndElement); ok {
+			if element.Name.Local == "plist" {
+				break
+			}
+		}
+	}
+
+	return nil
+}
+
+func decodeKey(d *xml.Decoder) (string, error) {
+	token, err := d.Token()
+	if err != nil {
+		return "", err
+	}
+
+	chardata, ok := token.(xml.CharData)
+	if !ok {
+		return "", errors.New("Invalid plist 'key'")
+	}
+
+	key := string(chardata)
+
+	token, err = d.Token()
+	_, ok = token.(xml.EndElement)
+	if !ok {
+		return "", errors.New("Missing plist </key> element")
+	}
+
+	return key, nil
+}
+
+func decodeValue(d *xml.Decoder, key string, p interface{}) error {
+	v := reflect.ValueOf(p)
+	s := v.Elem()
+
+	if s.Kind() == reflect.Struct {
+		N := s.NumField()
+
+		for i := 0; i < N; i++ {
+			f := s.Field(i)
+			t := s.Type().Field(i)
+			k := t.Name
+
+			if k == key {
+				if g, ok := decoders[t.Type]; ok {
+					if err := g(d, k, f); err != nil {
+						return err
+					}
+				} else {
+					panic(errors.New(fmt.Sprintf("Cannot decode  plist field with type '%v'", t.Type)))
+				}
+			}
+		}
+
+	} else {
+		panic(errors.New(fmt.Sprintf("Expecting struct, got '%v'", s.Kind())))
+	}
+
+	return nil
+}
+
+func decodeString(d *xml.Decoder, name string, f reflect.Value) error {
+	if !f.CanSet() {
+		return fmt.Errorf("Cannot set struct field '%s'", name)
+	}
+
+	for {
+		token, err := d.Token()
+		if err != nil {
+			return err
+		}
+
+		if element, ok := token.(xml.StartElement); ok {
+			if element.Name.Local == "string" {
+				break
+			}
+
+			return fmt.Errorf("Invalid plist XML element '%s' for field '%s': expected 'string'", element.Name.Local, name)
+		}
+	}
+
+	token, err := d.Token()
+	if err != nil {
+		return err
+	}
+
+	chardata, ok := token.(xml.CharData)
+	if !ok {
+		return fmt.Errorf("Invalid plist 'string' for '%s'", name)
+	}
+
+	f.SetString(string(chardata))
+
+	return nil
+}
+
+func decodeBool(d *xml.Decoder, name string, f reflect.Value) error {
+	if !f.CanSet() {
+		return fmt.Errorf("Cannot set struct field '%s'", name)
+	}
+
+	for {
+		token, err := d.Token()
+		if err != nil {
+			return err
+		}
+
+		if element, ok := token.(xml.StartElement); ok {
+			if element.Name.Local == "true" {
+				f.SetBool(true)
+				return nil
+			}
+
+			if element.Name.Local == "false" {
+				f.SetBool(false)
+				return nil
+			}
+
+			return fmt.Errorf("Invalid plist XML element '%s' for field '%s': expected 'string'", element.Name.Local, name)
+		}
+	}
+
+	return fmt.Errorf("Missing plist value for field '%s'", name)
+}
+
+func decodeInt(d *xml.Decoder, name string, f reflect.Value) error {
+	if !f.CanSet() {
+		return fmt.Errorf("Cannot set struct field '%s'", name)
+	}
+
+	for {
+		token, err := d.Token()
+		if err != nil {
+			return err
+		}
+
+		if element, ok := token.(xml.StartElement); ok {
+			if element.Name.Local == "integer" {
+				break
+			}
+
+			return fmt.Errorf("Invalid plist XML element '%s' for field '%s': expected 'integer'", element.Name.Local, name)
+		}
+	}
+
+	token, err := d.Token()
+	if err != nil {
+		return err
+	}
+
+	chardata, ok := token.(xml.CharData)
+	if !ok {
+		return fmt.Errorf("Invalid plist 'integer' for '%s'", name)
+	}
+
+	ivalue, err := strconv.ParseInt(string(chardata), 10, 64)
+	if err != nil {
+		return err
+	}
+
+	f.SetInt(ivalue)
+
+	return nil
+}
+
+func decodeStringArray(d *xml.Decoder, name string, f reflect.Value) error {
+	if !f.CanSet() {
+		return fmt.Errorf("Cannot set struct field '%s'", name)
+	}
+
+	for {
+		token, err := d.Token()
+		if err != nil {
+			return err
+		}
+
+		if element, ok := token.(xml.StartElement); ok {
+			if element.Name.Local == "array" {
+				break
+			}
+
+			return fmt.Errorf("Invalid plist XML element '%s' for field '%s': expected 'array'", element.Name.Local, name)
+		}
+	}
+
+	strings := []string{}
+
+	for {
+		token, err := d.Token()
+		if err != nil {
+			return err
+		}
+
+		if element, ok := token.(xml.EndElement); ok {
+			if element.Name.Local == "array" {
+				break
+			}
+		}
+
+		if element, ok := token.(xml.StartElement); ok {
+			if element.Name.Local != "string" {
+				return fmt.Errorf("Invalid plist XML array element '%s' for field '%s': expected 'string'", element.Name.Local, name)
+			}
+
+			token, err := d.Token()
+			if err != nil {
+				return err
+			}
+
+			chardata, ok := token.(xml.CharData)
+			if !ok {
+				return fmt.Errorf("Invalid plist 'string' for '%s'", name)
+			}
+
+			strings = append(strings, string(chardata))
+		}
+	}
+
+	values := reflect.MakeSlice(reflect.Indirect(f).Type(), len(strings), len(strings))
+
+	for i, s := range strings {
+		values.Index(i).SetString(s)
+	}
+
+	f.Set(values)
+
+	return nil
 }
--- src/uhppoted/encoding/plist/plist_test.go
+++ src/uhppoted/encoding/plist/plist_test.go
@@ -1,6 +1,7 @@
 package plist
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -58,15 +59,69 @@ func TestEncode(t *testing.T) {
 	bytes, err := Encode(p)
 
 	if err != nil {
-		t.Errorf("plist.Encode(%v) returned unexpected error: %v", p, err)
+		t.Errorf("plist.Encode returned unexpected error: %v", err)
 		return
 	}
 
 	if string(bytes) != XML {
-		t.Errorf("plist.Encode(%v) returned invalid XML: '%s'", p, string(bytes))
+		t.Errorf("plist.Encodereturned unexpected XML: '%s'", string(bytes))
 		return
 	}
 }
 
 func TestDecode(t *testing.T) {
+	p := struct {
+		Label             string
+		Program           string
+		WorkingDirectory  string
+		ProgramArguments  []string
+		KeepAlive         bool
+		RunAtLoad         bool
+		StandardOutPath   string
+		StandardErrorPath string
+		Integer           int
+	}{}
+
+	err := Decode([]byte(XML), &p)
+
+	if err != nil {
+		t.Errorf("plist.Decode returned unexpected error: %v", err)
+		return
+	}
+
+	if p.Label != "com.github.twystd.uhppoted" {
+		t.Errorf("plist.Decode returned unexpected string for 'Label' field: '%s'", p.Label)
+	}
+
+	if p.Program != "/usr/local/bin/uhppoted" {
+		t.Errorf("plist.Decode returned unexpected string for 'Program' field: '%s'", p.Program)
+	}
+
+	if p.WorkingDirectory != "/usr/local/var/uhppoted" {
+		t.Errorf("plist.Decode returned unexpected string for 'WorkingDirectory' field: '%s'", p.WorkingDirectory)
+	}
+
+	if !reflect.DeepEqual(p.ProgramArguments, []string{"--debug", "--verbose"}) {
+		t.Errorf("plist.Decode returned unexpected string array for 'ProgramArguments' field: '%v'", p.ProgramArguments)
+	}
+
+	if !p.KeepAlive {
+		t.Errorf("plist.Decode returned unexpected bool for 'KeepAlive' field: '%v'", p.KeepAlive)
+	}
+
+	if p.RunAtLoad {
+		t.Errorf("plist.Decode returned unexpected bool for 'RunAtLoad' field: '%v'", p.RunAtLoad)
+	}
+
+	if p.StandardOutPath != "/usr/local/var/log/com.github.twystd.uhppoted.log" {
+		t.Errorf("plist.Decode returned unexpected string for 'StandardOutPath' field: '%s'", p.StandardOutPath)
+	}
+
+	if p.StandardErrorPath != "/usr/local/var/log/com.github.twystd.uhppoted.err" {
+		t.Errorf("plist.Decode returned unexpected string for 'StandardErrorPath' field: '%s'", p.StandardErrorPath)
+	}
+
+	if p.Integer != 6521 {
+		t.Errorf("plist.Decode returned unexpected integer for 'Integer' field: '%v'", p.Integer)
+	}
 }
