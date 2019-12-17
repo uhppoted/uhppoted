340cf2d2c7f986178ab1376b79873c9279112107
Tony Seebregts <tony.seebregts@gmail.com>
2019-12-17 10:46:32 -0800

    uhppoted-mqtt: Reworked HOTP.counters to default to 'var' file

--- src/uhppoted-mqtt/config/config_darwin.go
+++ src/uhppoted-mqtt/config/config_darwin.go
@@ -1,7 +1,8 @@
 package config
 
 const secrets string = "/usr/local/etc/com.github.twystd.uhppoted/mqtt.hotp.secrets"
-const counters string = "/usr/local/etc/com.github.twystd.uhppoted/mqtt.hotp.counters"
 const users string = "/usr/local/etc/com.github.twystd.uhppoted/mqtt.permissions.users"
 const groups string = "/usr/local/etc/com.github.twystd.uhppoted/mqtt.permissions.groups"
+
+const counters string = "/usr/local/var/com.github.twystd.uhppoted/mqtt.hotp.counters"
 const eventIDs string = "/usr/local/var/com.github.twystd.uhppoted/mqtt.events.retrieved"
--- src/uhppoted-mqtt/config/config_linux.go
+++ src/uhppoted-mqtt/config/config_linux.go
@@ -1,7 +1,8 @@
 package config
 
 const secrets string = "/etc/uhppoted/mqtt.hotp.secrets"
-const counters string = "/etc/uhppoted/mqtt.hotp.counters"
 const users string = "/etc/uhppoted/mqtt.permissions.users"
 const groups string = "/etc/uhppoted/mqtt.permissions.groups"
+
+const counters string = "/var/uhppoted/mqtt.hotp.counters"
 const eventIDs string = "/var/uhppoted/mqtt.events.retrieved"
