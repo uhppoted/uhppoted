c3e44465bbb6d10ea4bcb067984e33b6e85b1e7f
Tony Seebregts <tony.seebregts@gmail.com>
2020-02-07 12:15:41 -0800

    uhppoted-mqtt: Skeleton implementation for daemonize/windows

--- src/uhppoted-mqtt/commands/undaemonize_windows.go
+++ src/uhppoted-mqtt/commands/undaemonize_windows.go
@@ -4,15 +4,25 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"golang.org/x/sys/windows/svc/eventlog"
+	"golang.org/x/sys/windows/svc/mgr"
+	"os"
+	"path/filepath"
+	"syscall"
 )
 
-var UNDAEMONIZE = Undaemonize{}
-
-type Undaemonize struct {
+var UNDAEMONIZE = Undaemonize{
+	name:    SERVICE,
+	workdir: workdir(),
+	logdir:  filepath.Join(workdir(), "logs"),
+	config:  workdir(),
 }
 
-func NewUndaemonize() *Undaemonize {
-	return &Undaemonize{}
+type Undaemonize struct {
+	name    string
+	workdir string
+	logdir  string
+	config  string
 }
 
 func (u *Undaemonize) Name() string {
@@ -24,7 +34,7 @@ func (u *Undaemonize) FlagSet() *flag.FlagSet {
 }
 
 func (u *Undaemonize) Description() string {
-	return fmt.Sprintf("Deregisters %s as a service/daemon", SERVICE)
+	return fmt.Sprintf("Deregisters %s from the list of Windows services", SERVICE)
 }
 
 func (u *Undaemonize) Usage() string {
@@ -35,12 +45,98 @@ func (u *Undaemonize) Help() {
 	fmt.Println()
 	fmt.Printf("  Usage: %s undaemonize\n", SERVICE)
 	fmt.Println()
-	fmt.Printf("    Deregisters %s from launchd as a service/daemon", SERVICE)
+	fmt.Printf("    Deregisters %s from the list of Windows services", SERVICE)
 	fmt.Println()
 }
 
 func (u *Undaemonize) Execute(ctx context.Context) error {
 	fmt.Println("   ... undaemonizing")
+
+	if err := u.unregister(); err != nil {
+		return err
+	}
+
+	if err := u.clean(); err != nil {
+		return err
+	}
+
+	fmt.Printf("   ... %s deregistered as a Windows service\n", SERVICE)
+	fmt.Printf(`
+   NOTE: Configuration files in %s,
+               working files in %s,
+               and log files in %s
+               were not removed and should be deleted manually
+`, filepath.Dir(u.config), u.workdir, u.logdir)
+	fmt.Println()
+
+	return nil
+}
+
+func (u *Undaemonize) unregister() error {
+	m, err := mgr.Connect()
+	if err != nil {
+		return err
+	}
+
+	defer m.Disconnect()
+
+	s, err := m.OpenService(u.name)
+	if err != nil {
+		return fmt.Errorf("service %s is not installed", u.name)
+	}
+
+	defer s.Close()
+
+	err = s.Delete()
+	if err != nil {
+		return err
+	}
+
+	err = eventlog.Remove(u.name)
+	if err != nil {
+		return fmt.Errorf("RemoveEventLogSource() failed: %s", err)
+	}
+
+	return nil
+}
+
+func (u *Undaemonize) clean() error {
+	files := []string{
+		filepath.Join(u.workdir, fmt.Sprintf("%s.pid", SERVICE)),
+	}
+
+	directories := []string{
+		u.logdir,
+		u.workdir,
+	}
+
+	for _, f := range files {
+		fmt.Printf("   ... removing '%s'\n", f)
+		if err := os.Remove(f); err != nil && !os.IsNotExist(err) {
+			return err
+		}
+	}
+
+	for _, dir := range directories {
+		fmt.Printf("   ... removing '%s'\n", dir)
+		if err := os.Remove(dir); err != nil && !os.IsNotExist(err) {
+			patherr, ok := err.(*os.PathError)
+			if !ok {
+				return err
+			}
+
+			syserr, ok := patherr.Err.(syscall.Errno)
+			if !ok {
+				return err
+			}
+
+			if syserr != syscall.ENOTEMPTY {
+				return err
+			}
+
+			fmt.Printf("   ... WARNING: could not remove directory '%s' (%v)\n", dir, syserr)
+		}
+	}
 
 	return nil
 }
